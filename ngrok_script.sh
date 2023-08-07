9cf862d451aa87a64eac2d495873a559308b67a6
delcode92 <101550592+delcode92@users.noreply.github.com>
2023-08-07 21:58:27 +0700

    Create ngrok_script.sh

---
+++ ngrok_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SERVER="127.0.0.1"
+PATH_PROJ="~/mk_serv"
+ 
+# cd to path
+cd $PATH_PROJ
+
+
+
+#run ngrok service
+ngrok tcp $SERVER:22 &
+
+sleep 6
+
+#run curl service > ngrok_json.txt
+curl 127.0.0.1:4040/api/tunnels > ngrok_json.txt & 
+
+
+sleep 6
+
+
+cd $PATH_PROJ
+
+
+git add --all
+git commit -m "update" 
+git push origin main 
+
+
+wait
